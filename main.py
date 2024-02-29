4ebd14979f2c34aa1168965aaea508faa7f5b4f3
JosephChataignon <joseph.chataignon@unibe.ch>
2024-02-29 12:35:45 +0100

    adapt for bitsandbytes

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ bnb_config = BitsAndBytesConfig(
     bnb_4bit_quant_type="nf4",
     bnb_4bit_compute_dtype=torch.bfloat16
 )
-model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=bnb_config, device_map={"":0})
+model = AutoModelForCausalLM.from_pretrained(model_name, quantization_config=bnb_config, device_map="auto")
 model.gradient_checkpointing_enable()
 model = prepare_model_for_kbit_training(model) #peft function
 loraconfig = LoraConfig(
