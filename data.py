d105ef9449526373b264abb403e42713efd3fb95
JosephChataignon <joseph.chataignon@unibe.ch>
2024-03-19 11:26:58 +0100

    minor changes

--- data.py
+++ data.py
@@ -15,15 +15,19 @@ def get_CHANGE_data(data_type):
             # the files from Google drive
             from google.colab import drive
             drive.mount('/content/drive')
-            working_dir = "/content/drive/MyDrive/Unibe/"
-            data_dir = working_dir
+            data_dir = "/content/drive/MyDrive/Unibe/"
         else:
             # from Ubelix container
-            working_dir = '/research_storage/'
-            data_dir = working_dir + 'Walser_data/'
+            data_dir = '/research_storage/Projekt_Change_LLM/Walser_data/'
         train_file = data_dir + "train_dataset.txt"
         test_file  = data_dir + "test_dataset.txt"
         return train_file, test_file
+    elif data_type.lower() == "max_planck":
+        data_dir = '/research_storage/Data_MaxPlanckInstitut/'
+        # in Data_MaxPlanckInstitut/output, there are folders named as seg87, seg86b, seg01
+        # and a maxplanckdata folder which I don't know the use of
+        # actual data is in the seg*/input/*.txt
+
 
 
 
