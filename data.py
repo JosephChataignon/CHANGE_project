5b62d9c4505d7a32221be91e34f0948926f14250
JosephChataignon <joseph.chataignon@unibe.ch>
2024-03-22 16:41:20 +0100

    automatic commit from pysbatch execution

--- data.py
+++ data.py
@@ -31,7 +31,7 @@ def get_CHANGE_data(data_type):
         #       for dir in ./seg*/; do echo $dir $(ls $dir/input/*.txt | xargs cat | wc -l); done
         train_folders = ["seg01","seg02","seg03","seg04","seg05","seg06","seg07","seg08","seg09","seg10","seg11a","seg11b","seg12","seg13","seg14","seg15","seg16","seg17","seg18","seg19","seg20","seg21","seg22","seg23","seg24","seg25","seg26","seg27","seg28","seg29","seg30","seg31","seg32","seg33","seg34","seg35","seg36","seg37","seg38","seg39","seg40","seg41","seg42","seg43","seg44","seg45","seg46","seg47","seg48","seg49","seg50","seg51","seg52","seg53","seg54","seg55","seg56","seg57","seg58","seg59","seg60","seg61","seg62","seg63","seg64","seg65","seg66","seg67","seg68","seg69","seg70",]
         test_folders = ["seg71","seg72","seg73","seg74","seg75","seg76","seg77","seg78","seg79",]
-        val_folders["seg80","seg81","seg82","seg83","seg84","seg85","seg86a","seg86b","seg87"]
+        val_folders = ["seg80","seg81","seg82","seg83","seg84","seg85","seg86a","seg86b","seg87"]
         # replace the "segXX" with a path to txt files, using a * wildcard
         for folders_list in [train_folders, test_folders, val_folders]:
             folders_list = [f"{data_dir}output/{seg}/input/{seg}*.txt" for seg in folders_list]
