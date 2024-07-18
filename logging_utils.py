2874e289b9cf8518ff91a18a1316093ea1c28c99
JosephChataignon <joseph.chataignon@unibe.ch>
2024-07-18 11:23:02 +0200

    automatic commit from pysbatch execution

--- logging_utils.py
+++ logging_utils.py
@@ -94,7 +94,6 @@ def print_trainable_parameters(model):
 
 
 def get_tb_callback(config,run_name):
-
     return CustomTensorBoardCallback(tb_dir=config['LOGS_FOLDER']+'/TensorBoard'+run_name)
 
 class CustomTensorBoardCallback(TensorBoardCallback):
