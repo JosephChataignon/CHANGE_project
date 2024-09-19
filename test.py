144aae3f0f0ea0d9fbb0397eb830fd2c3b4b86f6
JosephChataignon <joseph.chataignon@unibe.ch>
2024-09-19 14:41:18 +0200

    automatic commit from script execution

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import os, sys, logging, torch, transformers
 from dotenv import load_dotenv, dotenv_values
 from datetime import datetime, timedelta
-from datasets import load_metric
+#from datasets import load_metric
 
 from logging_utils import setup_logging, display_CUDA_info
 from data import get_CHANGE_data
@@ -15,10 +15,9 @@ assert 'SAVED_MODELS_DIR' in config, f'Could not find variable SAVED_MODELS_DIR 
 start_time = datetime.now()
 
 date_str = start_time.isoformat()[:19]
-log_file = f"{config['LOGS_FOLDER']}/{date_str}_{os.path.basename(__file__)}.log"
 root_logger = logging.getLogger()
 transformers_logger = transformers.logging.get_logger()
-setup_logging(log_file, root_logger, transformers_logger)
+setup_logging(config, root_logger, transformers_logger)
 
 
 logging.info(f"{start_time} - Imports finished, starting script\n\n")
@@ -35,4 +34,4 @@ train_file, test_file = get_CHANGE_data('Walser')
 
 logging.info(f'test & train files:{train_file},{test_file}')
 
-metric = load_metric("accuracy")
+#metric = load_metric("accuracy")
