90451e4b4ca68c3cd637d2c163d6bf8e410dea12
JosephChataignon <joseph.chataignon@unibe.ch>
2024-09-24 11:41:05 +0200

    automatic commit from script execution

--- models.py
+++ models.py
@@ -12,17 +12,16 @@ class truncatedLlama2(torch.nn.Module):
         super(truncatedLlama2, self).__init__()
         self.model = AutoModel.from_pretrained("meta-llama/Llama-2-7b-hf", use_auth_token=id_token, output_hidden_states=True)
         self.tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf", use_auth_token=id_token)
-        #self.model = torch.nn.Sequential(*list(self.model.children())[:3])  # Slice the model after the 3rd layer
+        # Truncate to first 3 layers
+        self.model.config.num_hidden_layers = 3
         self.model.layers = torch.nn.ModuleList(list(self.model.layers)[:3])
-        #self.fc = torch.nn.Linear(self.model.config.hidden_size, 4096)
 
-    def forward(self, input_ids, attention_mask=None, token_type_ids=None):
-        outputs = self.model(input_ids, attention_mask, token_type_ids)
-        hidden_states = outputs.hidden_states[3]  # Get the output of the 3rd layer
-        logits = self.fc(hidden_states)
-        return logits
-
-
+    def __getattr__(self, name):
+        """Delegate attribute access to the underlying model."""
+        try:
+            return super().__getattr__(name)
+        except AttributeError:
+            return getattr(self.model, name)
 
 
 
